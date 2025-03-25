a389ea23b2a98e7bd108ea981107730c900c53b5
Miguel Fernandez <miguelferlez@gmail.com>
2025-03-25 22:27:47 +0100

    feat: show database products in view

--- nodepop/controllers/homeController.js
+++ nodepop/controllers/homeController.js
@@ -3,6 +3,8 @@ import Product from '../models/Product.js';
 
 export async function index(req, res, next){
     try {
+        res.locals.products = await Product.find();
+
         res.render('home');
     } catch (err) {
         next(err);
--- nodepop/initDB.js
+++ nodepop/initDB.js
@@ -12,6 +12,28 @@ if(answer.toLowerCase() !== 'y') {
     process.exit();
 }
 
+await initProducts();
+await connection.close();
+
+async function initProducts() {
+    const deleteResult = await Product.deleteMany();
+    console.log(`Removed ${deleteResult.deletedCount} products!`);
+
+    const insertResult = await Product.insertMany([
+        { name: 'Lorem', price: 20},
+        { name: 'Ipsum', price: 13.99},
+        { name: 'Dolor sit', price: 9.95},
+        { name: 'Amet', price: 35},
+        { name: 'Consectetur', price: 66},
+        { name: 'Adiscing elit', price: 42.50},
+        { name: 'Duis dignissim', price: 50},
+        { name: 'Aliquet', price: 4},
+        { name: 'Pellentesque', price: 7.99},
+        { name: 'Vestibulum', price: 16},
+    ]);
+    console.log(`Inserted ${insertResult.length} products!`);
+}
+
 async function question(prompt) {
     const rl = readline.createInterface({
         input: process.stdin,
